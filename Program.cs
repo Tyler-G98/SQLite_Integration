e501a1fbfd3902cdd42179374d46175af1364c53
Tyler <tgartner062698@gmail.com>
2024-10-07 00:08:46 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿namespace SQLite_Integration;
+using System.Data.SQLite;
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string connectionString = "Data Source=database.db";
+        var connection = new SQLiteConnection(connectionString);
+
+        connection.Open();
+
+        string query;
+        var command = new SQLiteCommand(null, null);
+        try
+        {
+            query = "CREATE TABLE person (id INTEGER PRIMARY KEY, name TEXT NOT NULL, age INTEGER)";
+            command = new SQLiteCommand(query, connection);
+            command.ExecuteNonQuery();
+        }
+        catch (SQLiteException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+
+        query = "INSERT INTO person (name, age) VALUES ('@name', @age)";
+        command = new SQLiteCommand(query, connection);
+        command.Parameters.AddWithValue("@name", "John Doe");
+        command.Parameters.AddWithValue("@age", 30);
+        command.ExecuteNonQuery();
+
+        query = "SELECT * FROM person";
+        command = new SQLiteCommand(query, connection);
+        var reader = command.ExecuteReader();
+        while(reader.Read())
+        {
+            Console.WriteLine(reader["name"] + " " + reader["age"]);
+        }
+
+        query = "DELETE FROM person";
+        command = new SQLiteCommand(query, connection);
+        command.ExecuteNonQuery();
+
+        query = "SELECT * FROM person";
+        command = new SQLiteCommand(query, connection);
+        command.ExecuteNonQuery();
+        while (reader.Read())
+        {
+            Console.WriteLine(reader["name"] + " " + reader["age"]);
+        }
+    }
+}
+
+
---
+++ TableBuilder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace SQLite_Integration
+{
+    public class TableBuilder
+    {
+        #region Fields
+        private string _tableName;
+        private List<(string columnName, string columnType)> columns;
+        #endregion
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="tableName"></param>
+        public TableBuilder(string tableName)
+        {
+            _tableName = tableName;
+            columns = new List<(string columnName, string columnType)>();
+        }
+
+        public void AddColumn(string columnName, string columnType)
+        {
+            columns.Add((columnName, columnType));
+        }
+
+        public string BuildCreateTableQuery()
+        {
+            if ((columns.Count == 0) || string.IsNullOrEmpty(_tableName))
+            {
+                throw new InvalidOperationException("Table name and columns must be set before building the query");
+            }
+
+            string query = $"CREATE TABLE {_tableName} (";
+            for (int i = 0; i < columns.Count; i++)
+            {
+                query += $"{columns[i].columnName} {columns[i].columnType}";
+                if (i < columns.Count - 1)
+                {
+                    query += ", ";
+                }
+            }
+
+            query += ")";
+            return query;
+        }
+
+        public void CreateTable(SQLiteConnection connection)
+        {
+            string query = BuildCreateTableQuery();
+            var command = new SQLiteCommand(query, connection);
+            command.ExecuteNonQuery();
+        }
+    }
+}
